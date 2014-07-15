443ea1781f469879e783ccb8321e91411bc2f47a
Chris <christopherdieckhaus@gmail.com>
2014-07-15 18:09:06 -0400

    Random number click works, will work on adding prayers to page.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 var r = function getRandomInt(min, max) {
-		return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 
@@ -8,10 +8,18 @@ var r = function getRandomInt(min, max) {
 function AppViewModel() {
     this.firstName = ko.observable("Chris");
     this.lastName = ko.observable("Dieckhaus");
-    this.randomNumber = r(0,10);
+    this.randomNumber = ko.observable(0);
+    this.prayers = prayers["name"];
+    this.getRandomNumber = function(){
+      this.randomNumber(r(0,10));
+    };
+    
 }
 
 // Activates knockout.js
 $(document).ready(function() {
     ko.applyBindings(new AppViewModel());
 });
+
+var prayers = {"name":"Our Father",
+  "text":"Our Father, Who art in heaven, hallowed be Thy name; Thy kingdom come, Thy will be done on earth as it is in heaven. Give us this day our daily bread, and forgive us our trespasses, as we forgive those who trespass against us. And lead us not into temptation, but deliver us from evil. Amen."};
