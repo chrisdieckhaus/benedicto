ffbc81611f81b9aa71527f8ab03df5db8c3a6c31
Chris <christopherdieckhaus@gmail.com>
2014-07-14 22:35:47 -0400

    Playing with Javascript

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function Prayer() {
+  getRandomNumber = function(){
+    return 5;
+  }
+  
+}
+
+function getRandomInt(min, max) {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+};
