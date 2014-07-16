374d267d1c93edb6698817823f9cdb8189639320
Chris <christopherdieckhaus@gmail.com>
2014-07-16 16:22:35 -0400

    Add styled button for toggling show/hide text

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
+    $("#button-div").on('mouseenter',function(){
+      $(this).addClass("outer-hover");
+    });
     
-    $("button").on('click',function(){
+    $("#button-div").on('mouseleave',function(){
+      $(this).removeClass("outer-hover");
+      $(this).addClass("outer");
+    });
+    
+    $(".outer").on('click',function(){
         console.log("Click");
         $("#prayer").slideToggle();
     });
